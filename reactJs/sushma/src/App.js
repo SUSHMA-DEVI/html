f27a348957c6caaccf0631e7118c694771834d8d
SUSHMA-DEVI <21MH1A4246@acoe.edu.in>
2023-08-30 11:33:06 +0530

    first commit

---
+++ reactJs/sushma/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+import About from './About';
+
+function App() {
+  return (
+    <div>
+      {/*<h1> this is first app page</h1>*/}
+      <Home />
+      <About />
+    </div>
+  )
+}
+
+export default App;
